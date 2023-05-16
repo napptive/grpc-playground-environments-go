8aab9a97fea7e2824f1593bf575d6353ed2f8c81
hermione-napptive <cicd@napptive.com>
2023-05-16 08:24:52 +0000

    gpm automatic publish

--- entities.pb.validate.go
+++ entities.pb.validate.go
@@ -102,6 +102,35 @@ func (m *Environment) validate(all bool) error {
 
 	// no validation rules for StatusDetailMessage
 
+	if all {
+		switch v := interface{}(m.GetLastStatusCheck()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, EnvironmentValidationError{
+					field:  "LastStatusCheck",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, EnvironmentValidationError{
+					field:  "LastStatusCheck",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetLastStatusCheck()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return EnvironmentValidationError{
+				field:  "LastStatusCheck",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return EnvironmentMultiError(errors)
 	}
